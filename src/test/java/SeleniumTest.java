164c66d6aed6a0d96da8e4144ce218a55271debd
Alexander Sekunov <ansekunov@yandex.ru>
2021-05-23 21:05:16 +0300

    Added tests for the Web form #5

---
+++ src/test/java/SeleniumTest.java
@@ -0,0 +1,102 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+import org.junit.Test;
+import org.junit.Before;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.ExpectedCondition;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+/**
+ *
+ * @author Александр
+ */
+public class SeleniumTest {
+    WebDriver driver;
+    
+    @Before
+    public void setUp() {
+        if (System.getProperty("os.name").contains("Windows"))
+            System.setProperty("webdriver.chrome.driver", "ChromeDriver\\chromedriver.exe");
+        else
+            System.setProperty("webdriver.chrome.driver", "ChromeDriver/chromedriver");
+        System.out.println(System.getProperty("user.dir"));
+        driver = new ChromeDriver();
+    }
+    
+    @Test
+    public void СorrectExpressionInput() throws Exception {
+        driver.navigate().to("http://localhost:8080");        
+        driver.findElement(By.id("input")).sendKeys("2+(-5)*(7-8)");
+        driver.findElement(By.id("btnCalculate")).click();
+        (new WebDriverWait(driver, 10)).until(
+            (ExpectedCondition<Boolean>) (WebDriver d) -> d.findElement(By.id("output"))
+                .getAttribute("value").equals("2+(-5)*(7-8) = 7,000"));
+        driver.close();
+    }
+    
+    @Test
+    public void UncorrectExpressionInput() throws Exception {
+        driver.navigate().to("http://localhost:8080");        
+        driver.findElement(By.id("input")).sendKeys("hello");
+        driver.findElement(By.id("btnCalculate")).click();
+        (new WebDriverWait(driver, 10)).until(
+            (ExpectedCondition<Boolean>) (WebDriver d) -> d.findElement(By.id("output"))
+                .getAttribute("value").equals("Cинтаксическая ошибка!"));
+        driver.close();
+    }
+    
+    @Test
+    public void UndoLastAction() throws Exception {
+        driver.navigate().to("http://localhost:8080");        
+        driver.findElement(By.id("input")).sendKeys("1+2-3");
+        driver.findElement(By.id("btnCalculate")).click();
+        driver.findElement(By.id("input")).sendKeys("1*2/3");
+        driver.findElement(By.id("btnCalculate")).click();
+        driver.findElement(By.id("input")).sendKeys("1+2*3");
+        driver.findElement(By.id("btnCalculate")).click();
+        driver.findElement(By.id("btnCancel")).click();
+        (new WebDriverWait(driver, 10)).until(
+            (ExpectedCondition<Boolean>) (WebDriver d) -> d.findElement(By.id("input"))
+                .getAttribute("value").equals("1+2*3"));
+        driver.close();
+    }
+    
+    @Test
+    public void UndoManyActions() throws Exception {
+        driver.navigate().to("http://localhost:8080");        
+        driver.findElement(By.id("input")).sendKeys("1+2-3");
+        driver.findElement(By.id("btnCalculate")).click();
+        driver.findElement(By.id("input")).sendKeys("1*2/3");
+        driver.findElement(By.id("btnCalculate")).click();
+        driver.findElement(By.id("input")).sendKeys("1+2*3");
+        driver.findElement(By.id("btnCalculate")).click();
+        driver.findElement(By.id("btnCancel")).click();
+        driver.findElement(By.id("btnCancel")).click();
+        driver.findElement(By.id("btnCancel")).click();
+        (new WebDriverWait(driver, 10)).until(
+            (ExpectedCondition<Boolean>) (WebDriver d) -> d.findElement(By.id("input"))
+                .getAttribute("value").equals("1+2-3"));
+        driver.close();
+    }
+    
+    @Test
+    public void ClearOutput() throws Exception {
+        driver.navigate().to("http://localhost:8080");        
+        driver.findElement(By.id("input")).sendKeys("1+2-3");
+        driver.findElement(By.id("btnCalculate")).click();
+        driver.findElement(By.id("input")).sendKeys("1*2/3");
+        driver.findElement(By.id("btnCalculate")).click();
+        driver.findElement(By.id("input")).sendKeys("1+2*3");
+        driver.findElement(By.id("btnCalculate")).click();
+        driver.findElement(By.id("btnClear")).click();
+        (new WebDriverWait(driver, 10)).until(
+            (ExpectedCondition<Boolean>) (WebDriver d) -> d.findElement(By.id("output"))
+                .getAttribute("value").equals(""));
+        driver.close();
+    }
+}
